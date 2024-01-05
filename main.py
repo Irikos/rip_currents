8c0bcf1c396b06728374df983d70958b89cd8ba4
Andrei Dumitriu <andrei.bardezi@gmail.com>
2024-01-05 16:15:41 +0100

    update main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import helper_functions as hf
 
-
 videos_folder = "/home/andrei/Work/datasets/CVPR2023 Rip Current Segmentation: A Novel Benchmark and YOLOv8 Baseline Results/test videos/test videos"
 annotations_folder = "/home/andrei/Work/datasets/CVPR2023 Rip Current Segmentation: A Novel Benchmark and YOLOv8 Baseline Results/test videos/annotations"
 video_frames = "/home/andrei/Work/datasets/CVPR2023 Rip Current Segmentation: A Novel Benchmark and YOLOv8 Baseline Results/test videos/videos_frames (full)"
